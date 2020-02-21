4b4b8af30bd8eab645392d2c95369275a399201f
maxpittock <33483789+maxpittock@users.noreply.github.com>
2020-02-21 15:45:29 +0000

    Add files via upload
    Countries class added

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace Object_Oriented_Programming_Assessment_1
     {
         static void Main(string[] args)
         {
+            new Country() estonia = new Country("bob", 2); 
+        }
+    }
+    class Country{
+        public string name {get; set;}
+        public float popPercentile {get;set;}
+        public Tuple<string, string, string> vote {get; set;} = new Tuple <string, string, string>("yes", "no", "abstain");
+        public Country(string name_,float popPercentile_){
+            name = name_;
+            popPercentile = popPercentile_;
+        }
+        public voting(string vote_){
+            vote = vote_;
+
         }
     }
 }
