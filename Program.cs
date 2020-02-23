7ca18b6082912d315c3de85afc046e704b008b35
David166 <32456357+David166@users.noreply.github.com>
2020-02-23 17:18:24 +0000

    Made changes to the readFile function

--- Program.cs
+++ Program.cs
@@ -8,32 +8,48 @@ namespace Object_Oriented_Programming_Assessment_1
 {
     class Program
     {
+        static List<Country> countryList = new List<Country>();
         static void Main(string[] args)
         {
-            Country estonia = new Country("bob", 2);
-            Console.WriteLine($"{estonia.name} {estonia.popPercentile}");
+            // Country estonia = new Country("bob", 2);
+            // Console.WriteLine($"{estonia.name} {estonia.popPercentile}");
             string[] lines = readFile();    // Take an array of strings as 'lines'
             int count = 0;
-            foreach(string s in lines)
+
+            /*foreach(string s in lines)
             {
                 Console.WriteLine(lines[count]);
                 count++;
             }
-            Console.ReadKey();
+            Console.ReadKey();            
+            */
+            
         }
         static string[] readFile()  // Read the file Countries.txt and convert into an array of strings
         {
             try { 
-                 string[] lines = File.ReadAllLines("Countries.txt");
-                 return lines;
+                 string[] countries = File.ReadAllLines("Countries.txt");
+                 string[] population = File.ReadAllLines("popPercentage.txt");
+
+                 int count = 0;
+                 
+                foreach (string s in countries)
+                {
+                    Country a = new Country(countries[count], float.Parse(population[count]));
+                    countryList.Add(a);
+                    Console.WriteLine(countryList[count].name);
+                    count++;
+                }
+                Console.ReadKey();
+                 return null;
             }
             catch (FileNotFoundException)
             {
                 string[] temp = { "Countries.txt not found. Has it been moved/deleted?" }; 
                 return temp;
             }
-
         }
+
 
     }
     class Country{
@@ -44,8 +60,6 @@ namespace Object_Oriented_Programming_Assessment_1
             name = name_;
             popPercentile = popPercentile_;
         }
-    
-    
         public void voting(string vote_){ // We pass it the vote through the interface. There is no error checking here, as the interface will only accept <country> <yes, no, abstain>
           vote = vote_;
         }
