dc568188dc3440856b5d7d505fd8e249800b0b8c
David166 <32456357+David166@users.noreply.github.com>
2020-02-26 16:08:02 +0000

    Minor aesthetic changes.

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace Object_Oriented_Programming_Assessment_1
         static void Main(string[] args)
         {
             string[] lines = readFile();    // Take an array of strings as 'lines'
+            Console.WriteLine("The available commands are the following: \nvote <country> <yes:no:abstain>\nqualifiedMajority\nlistCountries\nautoVote\nSet the votes for each country using the aforementioned commands.\n");
             while (true)
             {
                 userInterface(Console.ReadLine());
@@ -25,7 +26,7 @@ namespace Object_Oriented_Programming_Assessment_1
         {
             try { // Proofing for if the file is not there
                  string[] countries = File.ReadAllLines("Countries.txt"); // Files are used as this data is dynamic. Brexit for example, has meant that this list needed to be changed.
-                 string[] population = File.ReadAllLines("popPercentage.txt"); // Pop percentage is used for storing the percentage, we dont ever end up using this, but it does add expandibility in the future.
+                 string[] population = File.ReadAllLines("popPercentage.txt"); // Pop percentage is used for storing the percentage, we don't ever end up using this, but it does add expandibility in the future.
 
                  int count = 0;
                  
@@ -59,7 +60,7 @@ namespace Object_Oriented_Programming_Assessment_1
                     int counter = 0;
                         foreach(Country countries in countryList) {
 
-                        if (countryList[counter].name == a[1]) // Provides no output if the country isnt found, but stops the user from entering a country that doesnt exist.
+                        if (countryList[counter].name == a[1]) // Provides no output if the country isn't found, but stops the user from entering a country that doesnt exist.
                         {
                             if (a[2] != "yes" && a[2] != "abstain" && a[2] != "no")
                             {
@@ -73,6 +74,9 @@ namespace Object_Oriented_Programming_Assessment_1
                         counter++;
                     }
                     break;
+                case ("autoVote"):
+                    autoVote();
+                    break;
                     case ("qualifiedMajority"):
                         Console.WriteLine(qualifiedMajority()); // Console.WriteLine is used to give a text output.
                         break;
@@ -80,11 +84,28 @@ namespace Object_Oriented_Programming_Assessment_1
                         listCountries();
                         break;
                     default:
-                        Console.WriteLine("Invalid. Acceptable commands are:\nvote <country> <yes:no:abstain>\nqualifiedMajority\nlistCountries"); // Shows the user all usable commands.
+                        Console.WriteLine("Invalid. Acceptable commands are:\nvote <country> <yes:no:abstain>\nqualifiedMajority\nlistCountries\nautoVote"); // Shows the user all usable commands.
                         break;
 
                 }
             }
+
+        static void autoVote()
+        {
+            int count = 0;
+            foreach (Country country in countryList)
+            {
+                Console.WriteLine($"{countryList[count].name} is voting:"); // For demonstration purposes
+                string a = Console.ReadLine();
+                if (a != "yes" && a != "abstain" && a != "no")
+                {
+                    Console.WriteLine("Invalid vote! Default: Abstain");
+                    a = "abstain";
+                }
+                countryList[count].vote = a;
+                count++;
+            }
+        }
         static void listCountries()
         {
             int count = 0;
@@ -109,9 +130,9 @@ namespace Object_Oriented_Programming_Assessment_1
             }
             int a = countryList.Count / 2; // If more than half the countries say yes, then its passes. Else, it has failed.
             if(a > vote_yes){
-                return "The vote has failed.";
+                return $"The vote has failed. The number of countries voting yes is {vote_yes}, and the number of countries voting no is {vote_no}.";
                 }else{
-                return "The vote has passed";
+                return $"The vote has passed. The number of countries voting yes is {vote_yes}, and the number of countries voting no is {vote_no}.";
                 }
         }
 
@@ -120,7 +141,7 @@ namespace Object_Oriented_Programming_Assessment_1
         public string name { get; set;}
         public float popPercentile { get; set; }
         public string vote { get; set; }
-        public Country(string name_, float popPercentile_){ // Automatically assumes Abstain. Its safer than assuming a vote of "yes" or "no"
+        public Country(string name_, float popPercentile_){ // Automatically assumes Abstain. It's safer than assuming a vote of "yes" or "no"
             name = name_;
             popPercentile = popPercentile_;
             vote = "abstain";
